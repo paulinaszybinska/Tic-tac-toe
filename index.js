9b7400fe8caa1002f22778b379298d0e4051ce1a
Paulina Szybinska <paulina.szybinska@gmail.com>
2021-08-27 11:00:00 +0200

    Add new array

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const fields = ["", "", "", "", "", "", "", "", ""];
 
 let activePlayer = "X";
 
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 fieldsElements.forEach((field) => {
   field.addEventListener("click", (e) => {
     const { pos } = e.target.dataset;
