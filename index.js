f7b46680ad65ce95ec46559e3ded7cdc56e558c2
Paulina Szybinska <paulina.szybinska@gmail.com>
2021-08-28 13:55:39 +0200

    New logic

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fieldsElements = document.querySelectorAll(".board__item");
+const panel = document.querySelector(".panel");
+const button = document.querySelector(".reset-button");
 
-const fields = ["", "", "", "", "", "", "", "", ""];
-
-let activePlayer = "X";
+let fields;
+let activePlayer;
+let gameActive;
 
 const winningConditions = [
   [0, 1, 2],
@@ -15,14 +17,78 @@ const winningConditions = [
   [2, 4, 6]
 ];
 
-fieldsElements.forEach((field) => {
-  field.addEventListener("click", (e) => {
-    const { pos } = e.target.dataset;
+const displayWinMessage = () => {
+  panel.innerText = `Congratulations ${activePlayer}. You won!`;
+};
 
-    if (fields[pos] === "") {
-      fields[pos] = activePlayer;
-      e.target.classList.add(`board__item--filled-${activePlayer}`);
-      activePlayer = activePlayer === "X" ? "O" : "X";
+const displayTieMessage = () => {
+  panel.innerText = `A tie in the game!`;
+};
+
+const clearMessage = () => {
+  panel.innerText = "";
+};
+
+const validateGame = () => {
+  let gameWon = false;
+  for (let i = 0; i <= 7; i++) {
+    const [posA, posB, posC] = winningConditions[i];
+    const value1 = fields[posA];
+    const value2 = fields[posB];
+    const value3 = fields[posC];
+
+    if (value1 !== "" && value1 === value2 && value1 === value3) {
+      gameWon = true;
+      break;
     }
+  }
+
+  if (gameWon) {
+    gameActive = false;
+    displayWinMessage();
+  } else if (isBoardFull()) {
+    gameActive = false;
+    displayTieMessage();
+  }
+};
+
+const isBoardFull = () => {
+  return fields.find((field) => field === "") === undefined;
+};
+
+const resetBoardClasses = () => {
+  fieldsElements.forEach((field) => {
+    field.classList.remove("board__item--filled-X", "board__item--filled-O");
   });
+};
+
+const handleButtonClick = () => {
+  setDefaults();
+  resetBoardClasses();
+  clearMessage();
+};
+
+const handleItemClick = (e) => {
+  const { pos } = e.target.dataset;
+
+  if (gameActive && fields[pos] === "") {
+    fields[pos] = activePlayer;
+    e.target.classList.add(`board__item--filled-${activePlayer}`);
+    validateGame();
+    activePlayer = activePlayer === "X" ? "O" : "X";
+  }
+};
+
+const setDefaults = () => {
+  fields = ["", "", "", "", "", "", "", "", ""];
+  activePlayer = "X";
+  gameActive = true;
+};
+
+button.addEventListener("click", handleButtonClick);
+
+fieldsElements.forEach((field) => {
+  field.addEventListener("click", handleItemClick);
 });
+
+setDefaults();
